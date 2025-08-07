5fa7698651f5281c192b680301b2e2cc7cf6ceda
Joydeb Gan Prokas <joydebganprokas@gmail.com>
2025-08-08 00:42:58 +0600

    home screen modified

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
-import '../app_theme.dart';
+import '../widgets/main_layout.dart';
+import '../pages/assistant_page.dart';
+import '../pages/appointments_page.dart';
+import '../pages/history_page.dart';
+import '../pages/nearby_page.dart';
+import '../controllers/location_controller.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late LocationController _locationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _locationController = LocationController();
+  }
+
+  @override
+  void dispose() {
+    _locationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DocLinker'),
-        backgroundColor: AppTheme.primaryColor,
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to DocLinker!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ),
+    return MainLayout(
+      pages: [
+        AssistantPage(locationController: _locationController),
+        AppointmentsPage(),
+        HistoryPage(),
+        NearbyPage(locationController: _locationController),
+      ],
     );
   }
 } 
